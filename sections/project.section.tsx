24bacd0cb049f9905824858db0e7b971aa3ee36b
Surya Basak <suryabasak10@gmail.com>
2024-06-08 14:54:16 +0530

    darken image bg

--- sections/project.section.tsx
+++ sections/project.section.tsx
@@ -8,13 +8,16 @@ export default function ProjectSection() {
                 {
                     projects.map((project, index) => (
                         <div key={index} className="portfolio-box">
-                            <img src={project.imageUrl} alt={project.title}/>
+                            <img src={project.imageUrl} alt={project.title} />
                             <div className="portfolio-layer">
                                 <h4 className="font-bold">{project.title}</h4>
                                 <p>{project.description}</p>
-                                {/* <a href={project.link}>
-                                    <i className='bx bx-link-external'></i>
-                                </a> */}
+                                {
+                                    project.link &&
+                                    <a href={project.link} target="_blank">
+                                        <i className='bx bx-link-external'></i>
+                                    </a>
+                                }
                             </div>
                         </div>
                     ))
