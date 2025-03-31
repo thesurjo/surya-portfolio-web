aeb0b8330e42323eb3d50ed9dbfc35e03f72dfa9
Surya Basak <suryabasak10@gmail.com>
2025-03-31 22:46:00 +0530

    Update resume PDF filename in iframe and remove outdated CV file

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
         <>
             <main className="font-jost hyphens-manual w-full h-screen">
-                <iframe className="w-full h-full" src="/files/SuryaBasak_CV_V3.pdf" />
+                <iframe className="w-full h-full" src="/files/SuryaBasak-Resume.pdf" />
             </main>
         </>
     );
