16fb5eeaa0d633a51fa8f36de6cb2a488a8f1250
Surya Basak <87497313+thesurjo@users.noreply.github.com>
2025-04-20 16:13:49 +0530

    Remove background image from project section for a cleaner design

--- sections/project.section.tsx
+++ sections/project.section.tsx
@@ -22,7 +22,7 @@ export default function ProjectSection() {
     return (
         <section className="relative w-full py-16 px-4" id="project">
             <div className="absolute inset-0">
-                <div className="absolute inset-0 bg-[url('/images/grid-pattern.svg')] opacity-5"></div>
+                <div className="absolute inset-0 opacity-5"></div>
                 <div className="absolute inset-0 bg-gradient-to-b from-[--main-color]/5 via-transparent to-[--main-color]/5"></div>
                 <div className="absolute inset-0 backdrop-blur-[1px]"></div>
             </div>
